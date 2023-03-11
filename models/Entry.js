c98607f14c606d4056f3d3996122ae28f806e5ee
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-03-11 16:57:39 -0500

    changed model name

--- models/Entry.js
+++ models/Entry.js
@@ -1,20 +1,20 @@
-//set blog model for connecting to database
+//set Entry model for connecting to database
 //responsible for handling data and business logic
 
-const { Model, DataTypes, STRING } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const SignUp = require("./Signup");
 
-class Blog extends Model {}
+class Entry extends Model {}
 
-Blog.init(
+Entry.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       autoIncrement: true,
     },
-    blog_name: {
+    entry_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -39,11 +39,11 @@ Blog.init(
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "Blog",
+    modelName: "Entry",
   }
 );
 
 // `sequelize.define` also returns the model
-console.log(Blog === sequelize.models.Blog); // true
+console.log(Entry === sequelize.models.Entry); // true
 
-module.exports = {Blog, SignUp};
+module.exports = {Entry, SignUp};
--- models/index.js
+++ models/index.js
@@ -1,27 +1,36 @@
-//Blog to User: one to many
-//User to SignUp: one to one
-//Blog to SignUp: many to many
+//Entry to User: one to one (one entry to one user)
+//User to Entry: one to many (one user to many entries)
+//User to SignUp: one to one (one user has one signUp)
+//Entry to SignUp: many to many 
 
 
-const Blog = require("./User");
+const Entry = require("./User");
 const User = require("./User");
 const SignUp = require("./SignUp");
+
+Entry.hasOne(User, {
+    foreignKey: "userId",
+});
+
+User.hasMany(Entry, {
+
+});
 
 SignUp.belongsTo(User, {
     foreignKey: "userID",
     onDelete: 'CASCADE',
 });
 
-Blog.hasMany(User, {
-    foreignKey: "userId",
-});
-
-Blog.hasMany(SignUp, {
-
-});
-
+//? Do I need this, if the above block is the same, but reversed?
 User.hasOne(SignUp, {
     
+}); 
+
+
+Entry.hasMany(SignUp, {
+
 });
 
-module.exports = {Blog, User, SignUp};
+
+
+module.exports = {Entry, User, SignUp};
