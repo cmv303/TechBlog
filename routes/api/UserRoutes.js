096c9da80754a6511129f445e96be875a0a7bf2e
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-03-11 12:05:54 -0500

    fixed typo

--- routes/api/UserRoutes.js
+++ routes/api/UserRoutes.js
@@ -4,10 +4,10 @@
 const express = require ('express');
 const router = express.Router();
 
-const {user} = require ('../../controllers');
-console.log("user", user);
-router.get('/:id', user.get);
-router.post('/', user.post);
-router.post('/signup', user.post);
+const {User} = require ('../../controllers');
+console.log("user", User);
+router.get('/:id', User.get);
+router.post('/', User.post);
+router.post('/signup', User.post);
 
 module.exports = router;
