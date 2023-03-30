42cd743712035f9fa974e2c0bde585c841418bc1
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-03-30 15:20:58 -0400

    got posts to render on dashboard and on homepage, albeit with some issues that still need to be worked out

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,20 +1,21 @@
 async function editFormHandler(event) {
   event.preventDefault();
-  const post_name = document.querySelector("#post_name").value;
-  const description = document.querySelector("#description").value;
-  const user_name = document.querySelector("#user_name").value;
+  const postId = event.target.getAttribute("data-post_id");
+  const editPostName = document.querySelector(`#editPostName${postId}`).value;
+  const editPostDescription = document.querySelector(
+    `#editPostDescription${postId}`
+  ).value;
 
   // window.location gives access to the URL. Rhe .split() method is used to access the number at the end of the URL, and set that equal to id.
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
+  // const id = window.location.toString().split("/")[
+  //   window.location.toString().split("/").length - 1
+  // ];
 
-  const response = await fetch(`/api/${id}`, {
+  const response = await fetch(`/edit/${id}`, {
     method: "PUT",
     body: JSON.stringify({
-      post_name,
-      description,
-      user_name,
+      post_name: editPostName,
+      description: editPostDescription,
     }),
     headers: {
       "Content-Type": "application/json",
@@ -23,12 +24,18 @@ async function editFormHandler(event) {
 
   // If the response is ok, post is updated successfully.
   if (response.ok) {
-    document.location.replace(`/post/${id}`);
+    document.location.reload();
   } else {
     alert("Failed to edit post");
   }
+  const editPostForm = document.querySelector(`#editPostForm${postId}`);
+if (editPostForm) {
+  editPostForm.addEventListener("submit", editFormHandler);
+}
 }
 
-document
-  .querySelector(".edit-post-form")
-  .addEventListener("submit", editFormHandler);
+
+
+// document
+//   .querySelector(".edit-post-btn-form")
+//   .addEventListener("click", editFormHandler);
