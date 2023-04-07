aa49c07f7c17d8a414b8eae29062ae30000ef54a
cmv303 <115678318+cmv303@users.noreply.github.com>
2023-04-07 17:57:18 -0400

    added delete functionality to dashboard

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -34,9 +34,9 @@ router.put('/:id', withAuth, async (req, res) => {
     }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete('/post/:id', withAuth, async (req, res) => {
     try {
-        const [affectedRows] = Post.destroy({
+        const affectedRows = await Post.destroy({
             where: {
                 id: req.params.id,
             },
--- public/js/indiv-post.js
+++ public/js/indiv-post.js
@@ -2,7 +2,7 @@ async function deletePostHandler(event) {
     event.preventDefault();
     const postId = event.target.getAttribute("data-post_id");
   
-    const response = await fetch(`api/new/${postId}`, {
+    const deletePost = await fetch(`api/new/post/${postId}`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -10,7 +10,7 @@ async function deletePostHandler(event) {
     });
   
     // If the response is ok, post is deleted successfully.
-    if (response.ok) {
+    if (deletePost.ok) {
       document.location.reload();
     } else {
       alert("Failed to delete post");
@@ -19,7 +19,7 @@ async function deletePostHandler(event) {
   
   document.querySelectorAll(".deletePost-btn").forEach((button) => {
     button.addEventListener("click", (event) => {
-      const postId = event.target.getAttribute("data-post_id");
+      postId = event.target.getAttribute("data-post_id");
       if (confirm("Are you sure you want to delete this post?")) {
         deletePostHandler(event);
       }
